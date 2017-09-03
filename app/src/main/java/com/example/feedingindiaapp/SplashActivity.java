5cec26e30315e87cfcc25b29624757cef74d0ac6
Abhishek Nigam <abhisheknigam1996@gmail.com>
2017-09-03 18:37:06 +0530

    Changed splash activity according to material spec

--- app/src/main/java/com/example/feedingindiaapp/SplashActivity.java
+++ app/src/main/java/com/example/feedingindiaapp/SplashActivity.java
@@ -3,41 +3,31 @@ package com.example.feedingindiaapp;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 
 public class SplashActivity extends AppCompatActivity {
 
-    private static int SPLASH_TIME_OUT = 1000;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
 
-        new Handler().postDelayed(new Runnable() {
 
-            @Override
-            public void run() {
+        SharedPreferences sharedPreferences = getSharedPreferences("app_data", MODE_PRIVATE);
+        Boolean isLoggedIn = sharedPreferences.getBoolean("is_logged_in", false);
 
-                SharedPreferences sharedPreferences = getSharedPreferences("app_data", MODE_PRIVATE);
-                Boolean isLoggedIn = sharedPreferences.getBoolean("is_logged_in", false);
+        if (isLoggedIn) {
 
-                if(isLoggedIn) {
+            Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
 
-                    Intent intent = new Intent(SplashActivity.this, MainActivity.class);
-                    startActivity(intent);
-                    finish();
+        } else {
 
-                } else {
+            Intent i = new Intent(SplashActivity.this, LoginActivity.class);
+            startActivity(i);
+            finish();
 
-                    Intent i = new Intent(SplashActivity.this, LoginActivity.class);
-                    startActivity(i);
-                    finish();
+        }
 
-                }
-
-            }
-        }, SPLASH_TIME_OUT);
     }
 }
